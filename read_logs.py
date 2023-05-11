fc9ae5483d4ffd2559745f8ee94bc82e37a08b3e
miajw <91989739+miajw@users.noreply.github.com>
2023-05-11 14:28:49 -0500

    Add files via upload

---
+++ read_logs.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 11 13:14:44 2023
+
+@author: miaweaver
+"""
+
+import pickle
+
+def read_log(file_name):
+    with open(file_name, 'rb') as handle:
+        log = pickle.load(handle)
+    return log
+
+
+file_names = ["logs/transactions.out", "logs/wallet_log.out", "logs/committed_blocks.out", "logs/tx_commit_times.out"]
+msgs = ["ORDERED TRANSACTIONS: ", "WALLET STATES: ", "COMMITTED BLOCKS: ", "COMMIT_TIMES: "]
+
+for i, file_name in enumerate(file_names):
+    log = read_log(file_name)
+    if len(log) == 0:
+        continue
+    if type(log[0]) == list:
+        for item in log:
+            print(item)
+    elif type(log) == dict:
+        for tx, commit_time in log.items():
+            print("tx:", tx)
+            print("commit_time:", commit_time)
+    else:
+        print(log)
+    print("\n")
